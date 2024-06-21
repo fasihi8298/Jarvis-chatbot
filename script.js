bc705b20cb34d664683c84b0b6ba215c8bd8bfeb
Fasihi Ali <153727111+fasihi8298@users.noreply.github.com>
2024-06-22 02:45:13 +0500

    readout function and checks

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // element //
 
-const startbtn = document.querySelector("#start");
-const stopbtn = document.querySelector("#stop");
+const startbtn = document.getElementById("startbtn");
+const stopbtn = document.getElementById("stopbtn");
+const speakbtn = document.getElementById("speakbtn");
 
 
 // Speech recognition setup //
 
 const SpeechRecognition = 
- window.SpeechRecognition || window.webkitSpeeechRecognition;
+ window.SpeechRecognition || window.webkitSpeechRecognition;
 
  const recognition = new SpeechRecognition();
 
@@ -16,27 +17,48 @@ const SpeechRecognition =
     console.log("vr active");
  };
 
+//  sr result //
+
+recognition.onresult = function(event){
+let current = event.resultIndex;
+let transcript = event.results[current][0].transcript;
+
+console.log(transcript);
+}   
+
  //  sr end //
  recognition.onend = function(){
     console.log("vr deactive");
  };
 
 //  sr continous //
-recognition.continous = true;
+recognition.continuous = true;
 
- startbtn.addEventListener("click", () => {
-    recognition.start();
- });
+    startbtn.addEventListener("click", () => {
+      recognition.start();
+    });
+  
+    stopbtn.addEventListener("click", () => {
+      recognition.stop();
+    });
 
- stopbtn.addEventListener("click", () => {
-    recognition.start();
- });
 
 // Jarvis speech //
-function readout(messsage){
-    const speech = new SpeechSynthesisUtterance()
-    messsage.text = messsage;
-    speech.volume = 1
-    Window.SpeechSynthesis.speak(speech)
-    console.log("speaking out")
+function readout(message){
+    if ('speechSynthesis' in window) {
+        const speech = new SpeechSynthesisUtterance();
+        const allvoices = speechSynthesis.getVoices();
+        speech.text = message;
+        speech.voice = allvoices[30];
+        speech.volume = 1;
+        window.speechSynthesis.speak(speech);
+        console.log("speaking out");
+    } else {
+        console.log("Speech synthesis not supported");
+    }
 }
+
+speakbtn.addEventListener("click", () =>{ 
+    readout("hi my dear boys ");
+});
+
