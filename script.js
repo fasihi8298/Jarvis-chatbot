0f044420af67955365aca2e61b651c1f504995ca
Fasihi Ali <153727111+fasihi8298@users.noreply.github.com>
2024-06-22 01:01:05 +0500

    voice recognition and speech function

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// element //
+
+const startbtn = document.querySelector("#start");
+const stopbtn = document.querySelector("#stop");
+
+
+// Speech recognition setup //
+
 const SpeechRecognition = 
- windowRecognition || window.webkitSpeeechRecognition;
- const Recognition = new SpeechRecognition();
+ window.SpeechRecognition || window.webkitSpeeechRecognition;
+
+ const recognition = new SpeechRecognition();
+
+//  sr start //
+ recognition.onstart = function(){
+    console.log("vr active");
+ };
+
+ //  sr end //
+ recognition.onend = function(){
+    console.log("vr deactive");
+ };
+
+//  sr continous //
+recognition.continous = true;
+
+ startbtn.addEventListener("click", () => {
+    recognition.start();
+ });
+
+ stopbtn.addEventListener("click", () => {
+    recognition.start();
+ });
+
+// Jarvis speech //
+function readout(messsage){
+    const speech = new SpeechSynthesisUtterance()
+    messsage.text = messsage;
+    speech.volume = 1
+    Window.SpeechSynthesis.speak(speech)
+    console.log("speaking out")
+}
